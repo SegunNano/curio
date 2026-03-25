5e6ee2d5ef0216deca8e255d1d2d0697a004ab30
oluuseguun <segunfadipe97@gmail.com>
2026-03-25 15:27:42 +0100

    redit configurations edit6

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,18 +1,10 @@
 import { Redis } from 'ioredis'
 
-const isDev= process.env.NODE_ENV===`development`
-
-const url = isDev
-? {
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-}
-: process.env.REDIS_URL
 // For BullMQ
-export const redisConnection = url
+export const redisConnection = process.env.REDIS_URL
 
 // For direct Redis operations (caching)
-const redis = new Redis(url)
+const redis = new Redis(process.env.REDIS_URL)
 
 redis.on('connect', () => console.log('Redis connected 🟢'))
 redis.on('error', (err) => console.error('Redis error:', err.message))
